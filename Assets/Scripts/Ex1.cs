521a406eb854e894f24aa875b7e3188ab42c9e99
ariadnetaaa04 <ariadna.perez.0401@gmail.com>
2022-12-13 12:57:27 +0100

    13/12

--- Assets/Scripts/Ex1.cs
+++ Assets/Scripts/Ex1.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Ex1 : MonoBehaviour
 {
+    private int totalCoins;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,19 @@ public class Ex1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Coins();
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log($"There is {totalCoins} coins in the box");
+        }
+    }
+
+    private void Coins()
+    {
+        totalCoins++;
     }
 }
---
+++ Assets/Scripts/Ex2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ex2 : MonoBehaviour
+{
+    public float cm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Convertor(cm);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private float Convertor(float centimetro)
+    { 
+        float inches=centimetro * 0.39370f;
+        
+        return inches;
+    }
+
+}
